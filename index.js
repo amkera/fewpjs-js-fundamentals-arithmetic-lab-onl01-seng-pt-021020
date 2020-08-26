fc0cdb0e241989e655859307f4635f1700365421
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-26 23:15:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 1; 
+let num2 = 62; 
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random() * 20) + 1;
+//random number between 1 and 20
+
+let mod = 104%10;
+
+let max = Math.max(1, 3, 5, 6, 8, 20)
